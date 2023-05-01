bd333b3d57e9b8f1ffc593260dee8304ba3a579a
Iakov Lilo <iakov.lilo@userware.onmicrosoft.com>
2023-05-01 18:57:58 +1000

    Add project files.

---
+++ OpenSilverAttachDetach/AttachDetachControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Navigation;
+
+namespace OpenSilverAttachDetach
+{
+    public partial class AttachDetachControl : UserControl
+    {
+        public AttachDetachControl()
+        {
+            this.InitializeComponent();
+        }
+
+        public override void INTERNAL_AttachToDomEvents()
+        {
+            base.INTERNAL_AttachToDomEvents();
+            Console.WriteLine("INTERNAL_AttachToDomEvents");
+        }
+
+        public override void INTERNAL_DetachFromDomEvents()
+        {
+            base.INTERNAL_DetachFromDomEvents();
+            Console.WriteLine("INTERNAL_DetachFromDomEvents");
+        }
+    }
+}
---
+++ OpenSilverAttachDetach/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace OpenSilverAttachDetach
+{
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // Enter construction logic here...
+        }
+
+        public void OnAddClick(object sender, EventArgs args)
+        {
+            SP2.Children.Add(new TemplatedButton());
+        }
+
+        public void OnRemoveClick(object sender, EventArgs args)
+        {
+            SP2.Children.Remove(SP2.Children.Last());
+        }
+    }
+}
